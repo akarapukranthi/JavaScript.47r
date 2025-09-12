b7d433fc75b11b4d1e66d26ba4efbbf4d21add46
akarapukranthi <akarapukranthi444@gmail.com>
2025-09-12 20:07:35 +0530

    bulb task

---
+++ bulb_on-off_task/script.js
@@ -0,0 +1,9 @@
+function bulbAction(){
+   let image_ele = document.getElementById("image")
+   image_ele.src="https://img.freepik.com/free-vector/realistic-light-bulb-with-electricity_23-2149129410.jpg?semt=ais_hybrid&w=740&q=80"
+}
+
+function bulboff(){
+    let image_ele = document.getElementById("image")
+   image_ele.src="https://www.kindpng.com/picc/m/99-999603_png-light-bulb-off-transparent-png.png"
+}
