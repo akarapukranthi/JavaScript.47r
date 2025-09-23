7c0667d489a963112d8864839180ff7d836b56a9
akarapukranthi <akarapukranthi444@gmail.com>
2025-09-23 21:19:41 +0530

    color picker game

---
+++ color_picker/script.js
@@ -0,0 +1,23 @@
+
+let btn0=document.getElementById("green_btn")
+
+let btn1=document.getElementById("yellow_btn")
+
+let btn2= document.getElementById("purple_btn")
+
+let btn3 = document.getElementById("pink_btn")
+
+arr=["green","yellow","purple","pink"]
+
+btn0.style.backgroundColor=arr[0]
+btn1.style.backgroundColor=arr[1]
+btn2.style.backgroundColor=arr[2]
+btn3.style.backgroundColor=arr[3]
+
+
+document.body.style.backgroundColor="black"
+
+
+function all_in_one(color){
+  document.body.style.backgroundColor=color
+}
